0302f44efa7660610944386413b8b0922c64df6a
Juan Carlos Vélez <jcv28745@gmail.com>
2026-05-19 15:16:51 -0500

    Inicio-Ej1

--- Ej1.py
+++ Ej1.py
@@ -1,2 +1,18 @@
 # Hola mundo
 print("Hola mundo")
+
+# Crea un programa para saber si un número es par o impar
+
+try:
+    # Solicitamos al usuario que ingrese un número
+    numero = int(input("Introduce un número entero: "))
+    
+    # Un número es par si el resto de su división entre 2 es 0
+    if numero % 2 == 0:
+        print(f"El número {numero} es par.")
+    else:
+        print(f"El número {numero} es impar.")
+except ValueError:
+    print("Error: Por favor, ingresa un número entero válido.")
+
+
